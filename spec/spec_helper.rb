9f7038b7e9e4e35b121e835755cb8b36e1a4aaad
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2009-09-18 20:19:47 +0200

    working on CI setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + "/../lib"))
-require 'simplabs/excellent'
+
+require 'rubygems'
 require 'spec'
+require 'simplabs/excellent'
 
 begin
   require 'ruby-debug'
