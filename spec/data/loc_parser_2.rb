d0c182ec21829031d0f6ea4651d50a2838e7ab71
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2009-10-14 21:08:57 +0200

    added some more specs for the loc parser

---
+++ spec/data/loc_parser_2.rb
@@ -0,0 +1,16 @@
+require 'simplabs/excellent/checks'
+require 'simplabs/excellent/parsing'
+require 'simplabs/excellent/formatters'
+require 'simplabs/excellent/runner'
+require 'rubygems'
+require 'sexp'
+
+module Simplabs #:nodoc:
+
+  module Excellent #:nodoc:
+
+    VERSION = '1.5.1'
+
+  end
+
+end
---
+++ spec/parsing/locparser_spec.rb
@@ -0,0 +1,53 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+require 'simplabs/excellent/locparser'
+
+describe Simplabs::Excellent::LOCParser do
+
+  describe '#count' do
+
+    [
+      {
+        :file     => File.expand_path(File.dirname(__FILE__) + '/../data/loc_parser_1.rb'),
+        :expected => {
+          :code    => 14,
+          :comment => 1,
+          :blank   => 6,
+          :total   => 21
+        }
+      },
+      {
+        :file     => File.expand_path(File.dirname(__FILE__) + '/../data/loc_parser_2.rb'),
+        :expected => {
+          :code    => 11,
+          :comment => 0,
+          :blank   => 5,
+          :total   => 16
+        }
+      },
+      {
+        :file     => File.expand_path(File.dirname(__FILE__) + '/../data/loc_parser_3.rb'),
+        :expected => {
+          :code    => 26,
+          :comment => 21,
+          :blank   => 15,
+          :total   => 62
+        }
+      }
+    ].each do |check|
+
+      it "should correctly count the lines for #{File.basename(check[:file])}" do
+        file = check[:file]
+        loc_parser = Simplabs::Excellent::LOCParser.new([file])
+        count = loc_parser.count
+        count[file][:code].should == check[:expected][:code]
+        count[file][:comment].should == check[:expected][:comment]
+        count[file][:blank].should == check[:expected][:blank]
+        count[file][:total].should == check[:expected][:total]
+      end
+
+    end
+
+  end
+
+end
