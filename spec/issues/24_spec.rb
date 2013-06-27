bb72d14ea8835af63b22ebdad835a2536d82fb83
Marco Otte-Witte <mottewitte@rim.com>
2013-06-27 11:19:35 +0200

    fixed spec description

--- spec/issues/24_spec.rb
+++ spec/issues/24_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe 'issue #42' do
+describe 'issue #24' do
 
   before(:each) do
     @excellent = Simplabs::Excellent::Runner.new(Simplabs::Excellent::Checks::SingletonVariableCheck.new)
